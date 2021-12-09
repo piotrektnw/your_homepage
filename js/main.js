44867a7f8438c1f022ec38aeb2647f886bbb36ba
@piotr.panek <piotrpanek89@outlook.com>
2021-12-09 16:56:38 +0100

    Select category fixed, to-do-list added

--- js/main.js
+++ js/main.js
@@ -3,24 +3,16 @@ import { API_KEY } from "./api.js";
 
 const websiteBackground = document.querySelector('.background');
 const setBgBtn = document.querySelector('.set-background');
-const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked')
-
-
-
-const chooseCategory = () => {
-    checkboxes.forEach((checkbox) => {
-        console.log(checkbox.checked);
-    })
-}
+const addBtn = document.querySelector('.add')
+const toDoListContainer = document.querySelector('.block__item--5')
 
 const setBackground = () => {
-
     let checked = document.querySelectorAll('input[type="checkbox"]:checked')
-    
+
     let background;
     let categoryNo;
     let number = Math.floor(Math.random() * 19)
-    checked.length > 0 ? categoryNo = Math.floor(Math.random() * (checked.length)) : categoryNo = Math.floor(Math.random() * (checked.length-1))
+    checked.length > 0 ? categoryNo = Math.floor(Math.random() * (checked.length)) : categoryNo = Math.floor(Math.random() * (checked.length - 1))
 
     // Check category name
     console.log(checked[categoryNo].name);
@@ -35,8 +27,31 @@ const setBackground = () => {
         })
 }
 
-setBgBtn.addEventListener('click', setBackground)
+const addNewTask = () => {
+    let inputValue = document.querySelector('.to-do-input').value;
+    let ulList = document.querySelector('.to-do-list__list')
+    let newTask = document.createElement('li')
+    newTask.innerHTML = inputValue
+    ulList.appendChild(newTask)
+    console.log(inputValue);
 
+    growToDoList()
+}
+
+
+const growToDoList = () => {
+
+    let currentHeight = toDoListContainer.offsetHeight
+    currentHeight = currentHeight + 24 + "px"
+    toDoListContainer.style.height = currentHeight
+
+    // Verify element height
+    console.log(currentHeight);
+}
+
+
+setBgBtn.addEventListener('click', setBackground);
+addBtn.addEventListener('click', addNewTask)
 // Background auto-change
-window.setInterval(setBackground, 50000)
-window.addEventListener('DOMContentLoaded', chooseCategory)
+window.setInterval(setBackground, 50000);
+
